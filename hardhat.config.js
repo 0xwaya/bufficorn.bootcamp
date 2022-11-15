8dbf2e17996d4b2ee7247c423d04bcd93288071e
e.mercado <admin@wayalabs.com>
2022-11-15 08:23:55 -0500

    commit to main

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,4 @@ module.exports = {
     forking: {
       url: "https://eth-mainnet.alchemyapi.io/v2/ERQvSzxxqKh8OqUkeHr3v6vLPzYL2LDc",
     }
-    const impersonatedSigner = await ethers.getImpersonatedSigner("0x1234567890123456789012345678901234567890");
-    await impersonatedSigner.sendTransaction(...);}
-};
+  }}
