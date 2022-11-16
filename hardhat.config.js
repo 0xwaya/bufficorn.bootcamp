6bb43cd023c46ebd39a9bd810cf063fcbbd6c8cc
e mercado <wayacoin@gmail.com>
2022-11-16 15:53:21 -0500

    delete config
    Signed-off-by: e mercado <wayacoin@gmail.com>

--- hardhat.config.js
+++
@@ -1,9 +0,0 @@
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.17",
-  hardhat: {
-    forking: {
-      url: "https://eth-mainnet.alchemyapi.io/v2/()",
-    }
-
-  }}
