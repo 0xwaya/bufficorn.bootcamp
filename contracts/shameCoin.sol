566021c6bd9ffadf007352a6a3d1b6a7fd2afd9e
e.mercado <admin@wayalabs.com>
2022-11-16 15:37:48 -0500

    commit to mail

--- contracts/shameCoin.sol
+++ contracts/shameCoin.sol
@@ -7,5 +7,14 @@ pragma solidity 0.8.17;
 
 contract VolcanoCoin is Ownable, ERC20 {
 
-    
+    uint256 supply = 10000;
+    event changeSupply(uint256 indexed);
+    event transferbb(address, uint256 address);
+
+    constructor(uint256 innitialSupply) ERC20 ("ShameCoin", "SHAME"){
+        
+    }
+
+
+
 }
