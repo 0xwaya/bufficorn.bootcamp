3a5a9e85e71f212f677de2db5a7a690ce60c165e
e.mercado <admin@wayalabs.com>
2022-11-16 15:30:48 -0500

    commit to master

---
+++ contracts/shameCoin.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT License
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+pragma solidity 0.8.17;
+
+contract VolcanoCoin is Ownable, ERC20 {
+
+    
+}
