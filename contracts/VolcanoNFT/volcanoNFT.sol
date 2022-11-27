284fcc5745b79ba64d5f9bd379ae9c4ce0628d35
e.mercado <admin@wayalabs.com>
2022-11-27 10:01:47 -0500

    commit to main

--- contracts/VolcanoNFT/volcanoNFT.sol
+++ contracts/VolcanoNFT/volcanoNFT.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT license
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
