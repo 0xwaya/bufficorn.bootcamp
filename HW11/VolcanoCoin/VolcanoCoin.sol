73a37b317a80c2d4be71f93840c2ee7fb68e095c
e.mercado <admin@wayalabs.com>
2022-11-27 13:34:42 -0500

    commit to main

--- HW11/VolcanoCoin/VolcanoCoin.sol
+++ HW11/VolcanoCoin/VolcanoCoin.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 pragma solidity 0.8.17;
 
 contract VolcanoCoin is ERC20 {
-    constructor(uint256 initialSupply) ERC20("VolcanoCoin", "FIRE") {
+    constructor(uint256 initialSupply) ERC20("VolcanoCoin", "HOT") {
         _mint(msg.sender, initialSupply);
     }
 }
